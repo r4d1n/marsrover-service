16116ad3c16e88be436b11bfe5d2447137d50ec2
Jared Radin <jared.radin@gmail.com>
2017-05-08 22:41:56 -0700

    replace in memory cache with redis

--- main.go
+++ main.go
@@ -4,22 +4,21 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
 
+	"github.com/garyburd/redigo/redis"
 	"github.com/gorilla/mux"
-	"github.com/patrickmn/go-cache"
 	"github.com/r4d1n/marsrover"
 )
 
 var mars *marsrover.Client
-var c *cache.Cache
 
 func init() {
 	mars = marsrover.NewClient(os.Getenv("NASA_API_KEY"))
-	c = cache.New(60*time.Minute, 60*time.Minute)
 }
 
 func main() {
@@ -40,31 +39,42 @@ func main() {
 }
 
 func getManifest(w http.ResponseWriter, r *http.Request) {
-	var data *marsrover.Manifest
+	var j []byte
 	rover := mux.Vars(r)["rover"]
 	key := fmt.Sprintf("manifest:%s", rover)
-	if x, found := c.Get(key); found {
+	conn, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	if reply, _ := conn.Do("GET", key); reply != nil {
 		fmt.Printf("manifest:%s is in the cache \n", rover)
-		data = x.(*marsrover.Manifest)
+		j = reply.([]byte)
 	} else {
 		fmt.Printf("manifest:%s is NOT in the cache \n", rover)
+		var data *marsrover.Manifest
 		var err error
 		data, err = mars.GetManifest(rover)
+		j, err = json.Marshal(data)
+		_, err = conn.Do("SET", key, j)
 		if err != nil {
 			fmt.Println(err)
 		}
-		c.Set(key, data, cache.DefaultExpiration)
 	}
-	json, err := json.Marshal(data)
 	if err != nil {
 		fmt.Println(err)
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(json)
+	w.Write(j)
 }
 
 func getImagesBySol(w http.ResponseWriter, r *http.Request) {
-	var data *marsrover.PhotoResponse
+	var j []byte
+	conn, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 	rover := mux.Vars(r)["rover"]
 	sol, err := strconv.Atoi(mux.Vars(r)["sol"])
 	if err != nil {
@@ -72,50 +82,57 @@ func getImagesBySol(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 	}
 	key := fmt.Sprintf("sol:%s:%d", rover, sol)
-	if x, found := c.Get(key); found {
+	if reply, _ := conn.Do("GET", key); reply != nil {
 		fmt.Printf("%s is in the cache \n", key)
-		data = x.(*marsrover.PhotoResponse)
+		j = reply.([]byte)
 	} else {
 		fmt.Printf("%s is NOT in the cache \n", key)
+		var data *marsrover.PhotoResponse
 		var err error
 		data, err = mars.GetImagesBySol(rover, sol)
+		j, err = json.Marshal(data)
+		_, err = conn.Do("SET", key, j)
 		if err != nil {
 			fmt.Println(err)
 			http.NotFound(w, r)
 		}
-		c.Set(key, data, cache.DefaultExpiration)
 	}
-	json, err := json.Marshal(data)
 	if err != nil {
 		fmt.Println(err)
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(json)
+	w.Write(j)
 }
 
 func getImagesByEarthDate(w http.ResponseWriter, r *http.Request) {
-	var data *marsrover.PhotoResponse
+	var j []byte
+	conn, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 	rover := mux.Vars(r)["rover"]
 	date := mux.Vars(r)["date"]
 	key := fmt.Sprintf("date:%s:%s", rover, date)
-	if x, found := c.Get(key); found {
+	if reply, _ := conn.Do("GET", key); reply != nil {
 		fmt.Printf("%s is in the cache \n", key)
-		data = x.(*marsrover.PhotoResponse)
+		j = reply.([]byte)
 	} else {
 		fmt.Printf("%s is NOT in the cache \n", key)
+		var data *marsrover.PhotoResponse
 		var err error
 		data, err = mars.GetImagesByEarthDate(rover, date)
+		j, err = json.Marshal(data)
+		_, err = conn.Do("SET", key, j)
 		if err != nil {
 			fmt.Println(err)
 			http.NotFound(w, r)
 		}
-		c.Set(key, data, cache.DefaultExpiration)
 	}
-	json, err := json.Marshal(data)
 	if err != nil {
 		fmt.Println(err)
 		http.NotFound(w, r)
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(json)
+	w.Write(j)
 }
